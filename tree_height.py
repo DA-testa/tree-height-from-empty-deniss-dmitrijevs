d9160e34d2ca680e7773c4e0cb1ef9f9faa121de
Deniss Dmitrijevs 221RDB322 <125139518+deniss-dmitrijevs@users.noreply.github.com>
2023-03-02 17:14:56 +0000

    implemented compute height

--- tree_height.py
+++ tree_height.py
@@ -9,19 +9,44 @@ def compute_height(n, parents):
     # Write this function
     max_height = 0
     # Your code here
-    return max_height
+    node_heights = numpy.zeros(n)
+
+    for node, parent in enumerate(parents):
+        if (node_heights[node] == 0):
+            node_heights[node] = node_heights[node] + 1
+        if node_heights[parent] <= node_heights[node]:
+            node_heights[parent] = node_heights[node] + 1
+
+    max_height = node_heights[0]
+    for node_height in node_heights[1:]:
+        if node_height > max_height:
+            max_height = node_height
+
+    return int(max_height)
 
 
 def main():
     # implement input form keyboard and from files
-    
     # let user input file name to use, don't allow file names with letter a
     # account for github input inprecision
-    
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
-    pass
+
+    input_type = input()
+
+    if "i" in input_type.lower():
+        node_count = int(input())
+        parent_list = [int(num) for num in input().split()]
+        print(compute_height(node_count, parent_list))
+    elif "f" in input_type.lower():
+        path = input()
+        if not "a" in path:
+            with open(path, 'r') as file:
+                lines = file.readLines()
+                node_count = int(lines[0])
+                parent_list = [int(num) for num in lines[1].split()]
+                print(compute_height(node_count, parent_list))
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
